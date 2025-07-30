5bddaebf9728943bb56991f6571cd14340a204e2
pushp987pb <157052528+pushp987pb@users.noreply.github.com>
2025-07-30 12:05:18 +0530

    added APIs/user-api.js

---
+++ backend/APIs/user-api.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const userApp = express.Router();
+
+// importing middleware
+const verifyToken = require('../Middlewares/verifyToken.js')
+
+// get express-async-handler to handle async errors
+const expressAsyncHandler = require('express-async-handler');
+
+//importing req handlers from controllers
+const { getUser , createUser,userLogin ,updateUser,getDonation,getRoomBooking} = require('../Controllers/user-controllers')
+
+//user CRUD Operations
+// get users
+userApp.get('/get-user/:username',expressAsyncHandler(getUser))
+
+// create user
+userApp.post('/create-user',expressAsyncHandler(createUser))
+
+// login user
+userApp.post('/login',expressAsyncHandler(userLogin))
+
+// update user
+userApp.put('/update-user',verifyToken,expressAsyncHandler(updateUser))
+
+// getting donation details 
+userApp.get('/get-donation/:username',expressAsyncHandler(getDonation))
+
+//getting roombooking list 
+userApp.get('/get-room-booking/:username',expressAsyncHandler(getRoomBooking))
+
+//export
+module.exports=userApp
